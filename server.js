30078c1441dcb0a3ffd6917b87a9f2c4520128d2
Abhay Chauhan <chauhanabhay88688@gmail.com>
2026-02-21 17:44:24 +0530

    connect db

--- server.js
+++ server.js
@@ -1,4 +1,9 @@
+require("dotenv").config();
+const connectDB = require("./src/config/db");
 const app = require("./src/app");
+
+//db call
+connectDB();
 
 app.listen(3000, () => {
   console.log("Server is running on PORT : 3000");

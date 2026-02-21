909095e335071376b3a68545d76a981afcbbb8cd
Abhay Chauhan <chauhanabhay88688@gmail.com>
2026-02-21 18:53:19 +0530

    createa api endpoints

--- src/app.js
+++ src/app.js
@@ -1,5 +1,14 @@
 const express = require("express");
+const authRouter = require("./routes/auth.route");
+const cookieParser = require("cookie-parser");
 
 const app = express();
 
+//middlwares
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+//api endpoints
+app.use("/api/auth", authRouter);
 module.exports = app;

2ba2212c3aa261890dfaebb76245128d4a7f9f8d
Abhay Chauhan <chauhanabhay88688@gmail.com>
2026-02-23 15:49:06 +0530

    create account route

---
+++ src/controllers/account.controller.js
@@ -0,0 +1,14 @@
+const Account = require("../models/account.model");
+
+const createAccount = async (req, res) => {
+  const user = req.user;
+  const account = await Account.create({
+    user: user._id,
+  });
+
+  res.status(201).json({
+    account,
+  });
+};
+
+module.exports = { createAccount };
